eb32b9abb53a694d9a992f722f944a8e76d000d7
15euec076 <32997235+15euec076@users.noreply.github.com>
2017-12-18 23:00:10 +0530

    Create ass2.c

---
+++ beginner level/ass2.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(void)
+{
+int a=18426;
+int b=463;
+float c=44.83;
+char x[30]="Sachin Ramesh Tendulkar";
+char y[20]="Little Master";
+printf("%s\n",x);
+printf("%s\n",y);
+printf("%d\n",a);
+printf("%.2f\n",c);
+printf("%d\n",b);
+return 0;
+}
